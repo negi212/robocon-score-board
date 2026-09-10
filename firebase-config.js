c04bbb563ccf21fba55e1d13377314f0967477fa
negi212 <186390248+negi212@users.noreply.github.com>
2026-09-10 13:35:34 +0900

    Firebase設定を更新し、必要な値を追加。新しいセッション情報をJSONファイルとして追加。

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,12 @@
 // console.firebase.google.com で取得した firebaseConfig の値を貼り付けてください。
 // 未設定のまま(PASTE_ME)だと自動でMQTTモードになります。
 const FIREBASE_CONFIG = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME",
-  databaseURL: "PASTE_ME",
-  projectId: "PASTE_ME",
-  storageBucket: "PASTE_ME",
-  messagingSenderId: "PASTE_ME",
-  appId: "PASTE_ME"
+  apiKey: "AIzaSyCU5f_uo0HPY7a8rLzpzXMg8vHxfT5IWA0",
+  authDomain: "robocon-score-board.firebaseapp.com",
+  databaseURL: "https://robocon-score-board-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "robocon-score-board",
+  storageBucket: "robocon-score-board.firebasestorage.app",
+  messagingSenderId: "711604109641",
+  appId: "1:711604109641:web:42bd960e8a21c61ec91a98",
+  measurementId: "G-T7B6G45JQP"
 };
